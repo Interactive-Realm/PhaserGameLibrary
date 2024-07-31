8d8cdc5ae0dccfbbd0855232908db0ec68b708ee
Mattis Riiskjær Bentsen <mattisrbentsen@gmail.com>
2024-07-31 14:51:27 +0200

    Added SaveTheOcean and Flappy to Games folder

---
+++ src/Games/SaveTheOcean/main.ts
@@ -0,0 +1,36 @@
+import { Boot } from './scenes/Boot';
+import { GameCountdown } from './scenes/GameCountdown';
+import { Game as MainGame } from './scenes/Game';
+import { AUTO, Game } from 'phaser';
+import { Preloader } from './scenes/Preloader';
+
+//  Find out more information about the Game Config at:
+//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+const config: Phaser.Types.Core.GameConfig = {
+    type: AUTO,
+    parent: 'game-container',
+    scene: [
+        Boot,
+        Preloader,
+        MainGame,
+        GameCountdown
+    ],
+    physics: {
+        default: 'arcade',
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        //autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: window.innerWidth,
+        height: window.innerHeight
+    }, 
+    backgroundColor: 0x7699c6,
+};
+
+const StartGame = (parent: string) => {
+
+    return new Game({ ...config, parent });
+
+}
+
+export default StartGame;
