57ff640e24ab0eb630e0a9c7d4daa7504d9de03a
Mattis Riiskjær Bentsen <mattisrbentsen@gmail.com>
2024-08-09 00:32:47 +0200

    Comment

--- src/assetData.ts
+++ src/assetData.ts
@@ -9,6 +9,12 @@ export function pathSelector(custom: boolean) {
     // Nej else er til at BasePattern er til at load de assets man putter ind i BasePattern når man laver spillet.
     // Men PhaserGameLibrary er også vores Phaser Work Invironment, så argumentet er at spillet, også dem til kunder, 
     // bliver lavet her, så hvorfor distribuerer vi ikke bare altid alle assets herfra og bruger de assets til at bygge til kunden med i BasePattern?
+
+
+    // -- // -- // -- // This is what was written in the Repo Commit on GitHub, it explains the solution:
+    // Reusable Repo that serves as Distribution Repo, so all assets from here are distributed to BasePattern that then uses these both for building 
+    // and for distribution to Vercel. Vercel in turn uses Assets as the folder name, so now we have solved the location alignment problem: 
+    // We now only need one location path: 'assets'.
     
     // Fordi når vi bygger til kunden fra BasePattern, 
     
