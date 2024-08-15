0fa4e603e8ced8ecd70710f7f681c1f8a4ee1b81
Christian <CStochholm@kami-io.com>
2024-08-16 01:14:36 +0200

    Updated PlayerPrefab on branch

--- src/Components/Prefabs/AS_Player.ts
+++ src/Components/Prefabs/AS_Player.ts
@@ -16,7 +16,27 @@ export default class PlayerPrefab extends Phaser.Physics.Arcade.Sprite
 		this.invincible = false;
 	}
 
-	public SetInvicible = () => {
+
+	// Called from game when player has been hit, deducted one HP and sets a post damage invincibility state for 3 seconds
+	PlayerHit(){
+		this.health -= 1
+		if(this.health == 0) {
+			
+			//this.setVisible(false)
+			this.destroy();
+			return;
+		};
+		this.PlayerInvincibility();
+	}
+
+	PlayerInvincibility(){
+        this.SetStateInvincible();																	// Set Invincibility boolean to the opposet of what it currently is																	
+        this.PlayerBlink();																			// Initial blink
+        this.scene.time.addEvent({delay: 500, callback: this.PlayerBlink, repeat: 6}) 				// Makes the player blink during the duration of the invincibility state
+        this.scene.time.addEvent({delay: 3000, callback: this.SetStateInvincible.bind(this)})		// Sets the invincibility state to false after 3 seconds
+    }
+
+	SetStateInvincible = () => {
 		this.invincible = !this.invincible;
 		console.log(this.invincible);
 	}
